d47eafc93d50421202e0cae52775ed80e1555d41
unknown <0935165@hr.nl>
2020-06-29 00:54:27 +0200

    Added todos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import Doelgroep from "./Components/Doelgroep";
 import Prototyping from "./Components/Prototyping";
 import Team from "./Components/Team";
 
+// Todo's 
+// - Add contentblock file
+// - Make functional component
+// - Look for comparisions (in all components) which you can make dynamic 
+// - Make an array with content 
+// - Render the contentblock(s) with the content
+
 function App() {
   return (
     <div className="App">
